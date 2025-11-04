9f241e2eac4b2b3b8f32e87edaad6d01dae1402e
KKB_Official <kkboahene003@gmail.com>
2025-11-04 22:01:16 +0000

    A redo

--- 1_users.py
+++ 1_users.py
@@ -4,16 +4,17 @@ from appwrite.services.databases import Databases
 
 db = Databases(client)
 
-ROLE_SUPERADMIN = "superadmin"
-ROLE_FARM_MANAGER = "farm_manager"
-ROLE_FARM_OWNER = "farm_owner"
-ROLE_CARETAKER = "caretaker"
-ROLE_FULFILLMENT = "fulfillment_manager"
-ROLE_PACKAGING = "packaging_supervisor"
-ROLE_QA = "quality_officer"
-ROLE_SALES_MANAGER = "sales_manager"
-ROLE_SALES_PERSON = "sales_person"
-ROLE_ACCOUNTANT = "accountant"
+SUPERADMIN = "superadmin"
+FARM_MANAGER = "farm_manager"
+FARM_OWNER = "farm_owner"
+CARETAKER = "caretaker"
+TECHNICIAN = "technician"
+FULFILLMENT = "fulfillment_manager"
+PACKAGING = "packaging_supervisor"
+QA = "quality_assurance_officer"
+SALES_MANAGER = "sales_manager"
+SALES_PERSON = "sales_person"
+ACCOUNTANT = "accountant"
 
 # define attributes
 result = db.create_string_attribute(
@@ -44,7 +45,7 @@ result = db.create_enum_attribute(
     database_id= db_id,
     collection_id= db_collection_id1,
     key="role",
-        elements=["superadmin", "farm_manager", "farm_owner", "caretaker", "fulfillment_manager", "packaging_supervisor", "quality_officer", "sales_manager", "sales_person", "accountant"],
+        elements=["superadmin", "farm_manager", "farm_owner", "caretaker", "technician", "fulfillment_manager", "packaging_supervisor", "quality_officer", "sales_manager", "sales_person", "accountant"],
     required= True
 )
 
