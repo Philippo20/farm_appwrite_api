aa65129cd18225daebd9491670c2fbf2370e3381
KKB_Official <kkboahene003@gmail.com>
2025-11-08 08:20:34 +0000

    Updated some endpoints

--- storage.py
+++ storage.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, APIRouter, HTTPException, status
+from fastapi import FastAPI, APIRouter, HTTPException, status, File, UploadFile
 from fastapi.responses import StreamingResponse
-from main import client, bucket_id
+from main import client, bucket_id, project_id, appwrite_endpoint, db_id
 from appwrite.services.storage import Storage
 from appwrite.input_file import InputFile
 from appwrite.id import ID
 from io import BytesIO
+from db import db
+from typing import Annotated
 
 st = Storage(client)
 
@@ -15,20 +17,6 @@ st = Storage(client)
 # )
 
 storage_router = APIRouter(tags=["Storage"])
-
-@storage_router.post("/storage/buckets/{bucket_id}/files")
-def create_file():
-    with open("C:/Users/KKB_Official/Downloads/Microsoft_Cloud_Skill_Report.pdf", "rb") as f:
-        content = f.read()
-
-    result = st.create_file(
-        bucket_id=bucket_id,
-        file=InputFile.from_bytes(content, "file.pdf"),
-        file_id=ID.unique(),
-        permissions=[]
-    )
-   
-    return result
 
 
 @storage_router.get("/storage/buckets/{bucket_id}/files/{file_id}/download")
@@ -52,7 +40,7 @@ def get_file_for_download(file_id:str):
     except Exception as e:
         return {"error": str(e)}
 
-@storage_router.get("/storage/buckets/{bucket_id}/files/{file_id}")
+@storage_router.get("/storage/buckets")
 def list_files():
     result = st.list_files(
         bucket_id=bucket_id,
@@ -60,14 +48,53 @@ def list_files():
     )
     return result
 
-@storage_router.put("/storage/buckets/{bucket_id}/files/{file_id}")
+@storage_router.get("/storage/buckets/{bucket_id}/files/{file_id}")
 def get_file(file_id:str):
     result = st.get_file(
         bucket_id=bucket_id,
-        file_id=file_id
+        file_id=file_id,
         # permissions=[]
     )
     return result
+
+@storage_router.put("/storage/buckets/{bucket_id}/files")
+async def create_file(
+    file_upload: Annotated[UploadFile, File(...)]
+
+):
+    file_bytes = await file_upload.read()
+    
+    try:
+        # Upload file to Appwrite Storage
+        uploaded_file = st.create_file(
+            bucket_id=bucket_id,
+            file_id=ID.unique(),
+            file=InputFile.from_bytes(file_bytes, filename=file_upload.filename)
+            # file=crop_image.file  # use the file object directly
+        )
+        file_id = uploaded_file["$id"]
+
+        # Generate file URLs
+        view_url = f"{appwrite_endpoint}/storage/buckets/{bucket_id}/files/{file_id}/view?project={project_id}"
+        download_url = f"{appwrite_endpoint}/storage/buckets/{bucket_id}/files/{file_id}/download?project={project_id}"
+
+        # Save URLs to Appwrite Database
+        saved_doc = db.create_document(
+            database_id=db_id,
+            # collection_id=db_collection_id16,
+            document_id=ID.unique(),
+            data={
+                "crop_image": file_upload.filename}
+        )
+        return {
+            "message": "File created successfully",
+            "file_id": file_id,
+            "view_url": view_url,
+            "download_url": download_url,
+            "db_document_id": saved_doc["$id"]
+        }
+    except Exception as e:
+        return {"error": str(e)}
 
 @storage_router.delete("/storage/buckets/{bucket_id}/files/{file_id}")
 def delete_file(file_id:str):
